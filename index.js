e9c541d3a17f8823e3ef7e94dded0d273b6a08f4
Victoria Breen <victoriabreen8@hotmail.com>
2024-09-13 18:31:36 -0230

    Minor changes

--- index.js
+++ index.js
@@ -2,19 +2,30 @@
 
 const process = require("node:process");
 const args = process.argv.slice(2);
-const length = 8; // Password can only be max 8 chars
 
-function passwordGenerate(length) {
+let length = 8; // Password can only be max 8 chars
+let includesNums = false;
+let includesSpecial = false;
+let includesUpperCase = false;
+
+function passwordGenerate(length, chars) {
   // function to generate a random password
-  const chars = "abcdefghijklmnopqrstuvwxyz";
   let password = "";
 
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * chars.length); //
     password += chars[randomIndex];
   }
-
   return password;
+}
+
+function charSet() {
+  let chars = "abcdefghijklmnopqrstuvwxyz";
+  if (includesNums) chars += "0123456789";
+  if (includesSpecial) chars += "!@#$%^&*()-=+}{[];:,.?";
+  if (includesUpperCase) chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  return chars;
 }
 
 function printHelpMessage() {
@@ -23,30 +34,42 @@ function printHelpMessage() {
     Usage:
 
         --length=8    Set the password length (8 is the default characters)
-        --help, -h    Displays the help message `);
+        --help, -h    Displays the help message 
+        --numbers     Allows numbers in the password
+        --special     Allow special characters in the password
+        --uppercase   Allow uppercase letters in the password`);
 }
 
-// need to check for length
-args.forEach((args) => {
-  if (args.startsWith("--length")) {
+args.forEach((arg) => {
+  if (arg.startsWith("--length")) {
     const len = arg.split("=")[1];
-    if (!isNaN(len) && parseInt(len, 10) > 0) {
+    if (!isNaN(len) && parseInt(len, 10) >= 8) {
+      // at LEAST 8
       // double check
       length = parseInt(len, 10);
     } else {
       console.error("Error! Invaild length. Please provide a length of 1-9.");
       process.exit(1);
     }
+  } else if (arg === "--numbers") {
+    // flags
+    includesNums = true;
+  } else if (arg === "--special") {
+    includesSpecial = true;
+  } else if (arg === "--uppercase") {
+    includesUpperCase = true;
+  } else if (arg === "--help" || arg === "-h") {
+    printHelpMessage();
+    process.exit(0);
+  } else {
+    console.error("Error! Invaild argument: " + arg);
+    printHelpMessage();
+    process.exit(1);
   }
 });
 
-// need to check for help
-if (args.includes("--help") || args.includes("-h")) {
-  // works
-  // gives user options for help
-  printHelpMessage();
-}
-
 // Display the password
-const password = passwordGenerate(length);
+const charOptions = charSet();
+const password = passwordGenerate(length, charOptions);
+
 console.log(`Generated password: ${password}`);
