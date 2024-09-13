13fe5d65ca0ab3eb882a44e571aa2e5c546faad3
Victoria Breen <victoriabreen8@hotmail.com>
2024-09-13 18:02:04 -0230

    Checking for args

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let includesNums = false;
 let includesSpecial = false;
 let includesUpperCase = false;
 
-function passwordGenerate(length) {
+function passwordGenerate(length, chars) {
   // function to generate a random password
   let password = "";
 
@@ -20,16 +20,27 @@ function passwordGenerate(length) {
   return password;
 }
 
+function charSet() {
+  let chars = "abcdefghijklmnopqrstuvwxyz";
+  if (includesNums) chars += "0123456789";
+  if (includesSpecial) chars += "!@#$%^&*()-=+}{[];:,.?";
+  if (includesUpperCase) chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  return chars;
+}
+
 function printHelpMessage() {
   // Help message for the users if needed
   console.log(`
     Usage:
 
         --length=8    Set the password length (8 is the default characters)
-        --help, -h    Displays the help message `);
+        --help, -h    Displays the help message 
+        --numbers     Allows numbers in the password
+        --special     Allow special characters in the password
+        --uppercase   Allow uppercase letters in the password`);
 }
 
-// need to check for length
 args.forEach((args) => {
   if (args.startsWith("--length")) {
     const len = arg.split("=")[1];
@@ -40,16 +51,25 @@ args.forEach((args) => {
       console.error("Error! Invaild length. Please provide a length of 1-9.");
       process.exit(1);
     }
+  } else if (arg === "--numbers") {
+    // flags
+    includesNums = true;
+  } else if (arg === "--special") {
+    includesSpecial = true;
+  } else if (arg === "--uppercase") {
+    includesUpperCase = true;
+  } else if (arg === "--help" || arg === "-h") {
+    printHelpMessage();
+    process.exit(0);
+  } else {
+    console.error("Error! Invaild argument: " + arg);
+    printHelpMessage();
+    process.exit(1);
   }
 });
 
-// need to check for help
-if (args.includes("--help") || args.includes("-h")) {
-  // works
-  // gives user options for help
-  printHelpMessage();
-}
-
 // Display the password
-const password = passwordGenerate(length);
+const charOptions = charSet();
+const password = passwordGenerate(length, charOptions);
+
 console.log(`Generated password: ${password}`);
